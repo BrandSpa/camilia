900052cf39aafc98297c3c267af0a0253dcf77f0
alejandro sanabria <alejandro@brandspa.com>
2014-03-05 16:26:04 -0500

    add link to line boutique

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -69,7 +69,9 @@
 
 <div class="row">
 	<div class="col-lg-4 img-container">
+	<a href="/linea-boutique">
 		<img src="[[ asset('img/img-title-lb.svg') ]]" class="img-responsive">
+	</a>
 	</div>
 	<div class="flexslider section-slider col-lg-8">
 		<ul class="slides">

ea5eb8d5d2c60993f955fe143fab3b495b212344
Alejandro Sanabria <alejandrosanabria7@gmail.com>
2014-03-07 17:36:20 -0500

    change style line

---
+++ app/views/products_boutique.blade.php
@@ -0,0 +1,106 @@
+<!-- top columns -->
+<div class="col-lg-4 col-md-4  section-left" >
+	<img src="[[ asset('img/img-lb-1.png') ]]" class="img-responsive">
+	<span class="arrow-bottom visible-lg"></span>
+	<span class="arrow-right visible-md"></span>
+	<span class="arrow-bottom visible-sm visible-xs"></span>
+</div>
+
+<div class="col-lg-4 col-md-8 gallery-text" >
+	<h3>SAINT HONORE</h3>
+	<p>
+		El dulce es uno de los cinco sabores básicos y de los únicos que es aceptado de manera global por todas las culturas
+	</p>
+</div>
+
+<div class="col-lg-4 col-md-4  section-right" >
+	<img src="[[ asset('img/img-lb-2.png') ]]" class="img-responsive">
+	<span class="arrow-left visible-lg"></span>
+	<span class="arrow-right visible-md"></span>
+	<span class="arrow-bottom visible-sm visible-xs"></span>
+</div>
+<!-- end top columns -->
+
+<!-- center columns -->
+<div class="col-lg-4 col-md-8 section-left gallery-text" >
+	<h3>SAINT HONORE</h3>
+	<p>
+		El dulce es uno de los cinco sabores básicos y de los únicos que es aceptado de manera global por todas las culturas
+	</p>
+</div>
+<div class="col-lg-4 hidden-md hidden-sm hidden-xs">
+	<img src="[[ asset('img/img-lb-3.png') ]]" class="img-responsive">
+</div>
+
+<div class="col-lg-4 col-md-8 section-right gallery-text" >
+	<h3>SAINT HONORE</h3>
+	<p>
+		El dulce es uno de los cinco sabores básicos y de los únicos que es aceptado de manera global por todas las culturas
+	</p>
+</div>
+<!-- end center columns -->
+
+<!-- bottom columns -->
+<div class="col-lg-4 col-md-4" >
+	<img src="[[ asset('img/img-lb-4.png') ]]" class="img-responsive">
+	<span class="arrow-right visible-lg"></span>
+	<span class="arrow-left visible-md"></span>
+	<span class="arrow-top visible-sm visible-xs"></span>
+</div>
+
+<div class="col-lg-4 col-md-8 gallery-text">
+	<h3>OPERA</h3>		
+	<p>
+		Postre emblemático francés en donde se maridan el café y el chocolate en diferentes texturas.
+	</p>
+</div>
+
+<div class="col-lg-4 col-md-4" >
+	<img src="[[ asset('img/img-lb-5.png') ]]" class="img-responsive">
+	<span class="arrow-top visible-lg"></span>
+	<span class="arrow-left visible-md"></span>
+	<span class="arrow-top visible-sm visible-xs"></span>
+</div>
+<!-- end bottom columns -->
+
+<!-- center columns -->
+<div class="col-lg-4 col-md-8 section-left gallery-text" >
+	<h3>SAINT HONORE</h3>
+	<p>
+		El dulce es uno de los cinco sabores básicos y de los únicos que es aceptado de manera global por todas las culturas
+	</p>
+</div>
+<div class="col-lg-4 hidden-md hidden-sm hidden-xs">
+	<img src="[[ asset('img/img-lb-3.png') ]]" class="img-responsive">
+</div>
+
+<div class="col-lg-4 col-md-8 section-right gallery-text" >
+	<h3>SAINT HONORE</h3>
+	<p>
+		El dulce es uno de los cinco sabores básicos y de los únicos que es aceptado de manera global por todas las culturas
+	</p>
+</div>
+<!-- end center columns -->
+
+<!-- top columns -->
+<div class="col-lg-4 col-md-4  section-left" >
+	<img src="[[ asset('img/img-lb-1.png') ]]" class="img-responsive">
+	<span class="arrow-bottom visible-lg"></span>
+	<span class="arrow-right visible-md"></span>
+	<span class="arrow-bottom visible-sm visible-xs"></span>
+</div>
+
+<div class="col-lg-4 col-md-8 gallery-text" >
+	<h3>SAINT HONORE</h3>
+	<p>
+		El dulce es uno de los cinco sabores básicos y de los únicos que es aceptado de manera global por todas las culturas
+	</p>
+</div>
+
+<div class="col-lg-4 col-md-4  section-right" >
+	<img src="[[ asset('img/img-lb-2.png') ]]" class="img-responsive">
+	<span class="arrow-left visible-lg"></span>
+	<span class="arrow-right visible-md"></span>
+	<span class="arrow-bottom visible-sm visible-xs"></span>
+</div>
+<!-- end top columns -->	

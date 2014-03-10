6404a2187dd01f4454833f6e08f1b7f5a6c8b44b
Alejandro Sanabria <alejandrosanabria7@gmail.com>
2014-03-10 15:18:53 -0500

    fix slider header section

--- app/views/contact.blade.php
+++ app/views/contact.blade.php
@@ -3,20 +3,7 @@
 @stop
 
 @section('header')
-<div class="main-slider">
-
-	<div class="flexslider main-slider">
-		<img src="[[ asset('img/logo-section.svg') ]]" class="logo-section-slider">
-		<ul class="slides">
-			<li>
-				<img src="[[ asset('img/header.png') ]]" />
-			</li>
-			<li>
-				<img src="[[ asset('img/header.png') ]]" />
-			</li>
-		</ul>
-	</div>
-</div>
+@include('sections._slider_header')
 @stop
 @section('content')
 	@include('sections._contact')
---
+++ app/views/sections/_slider_header.blade.php
@@ -0,0 +1,15 @@
+<div class="main-slider">
+	<div class="flexslider main-slider">
+	<div class="container" style="position:relative">
+		<img src="[[ asset('img/logo-section.svg') ]]" class="logo-section-slider">
+	</div>
+		<ul class="slides">
+			<li>
+				<img src="[[ asset('img/header.png') ]]" />
+			</li>
+			<li>
+				<img src="[[ asset('img/header.png') ]]" />
+			</li>
+		</ul>
+	</div>
+</div>

50451644c4c58773e84badef579bb7dc52d1f010
Alejandro Sanabria <alejandrosanabria7@gmail.com>
2014-03-10 18:45:46 -0500

    added link to lounge te

--- app/views/sections/_btns_ct.blade.php
+++ app/views/sections/_btns_ct.blade.php
@@ -3,7 +3,7 @@
 			<a href="/el-chef" class="btn-chef"><span>Camilia · El Chef</span></a>
 		</div>
 		<div class="col-lg-6 section-right">
-			<a href="#" class="btn-te"><span>El Salón de Té</span></a>
+			<a href="/el-salon-de-te" class="btn-te"><span>El Salón de Té</span></a>
 
 		</div>
 	</div>

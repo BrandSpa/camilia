cb2bf2842df29577fe074d733654da37fc74c1cd
Alejandro Sanabria <alejandrosanabria7@gmail.com>
2014-03-10 12:42:27 -0500

    add link on btn petit

--- app/views/sections/_btn_petit.blade.php
+++ app/views/sections/_btn_petit.blade.php
@@ -1,6 +1,8 @@
 <div class="row">
 	<div class="col-lg-4 img-container">
+	<a href="/linea-petit">
 		<img src="[[ asset('img/img-title-lp.svg') ]]" class="img-responsive">
+	</a>
 	</div>
 	<div class="flexslider section-slider col-lg-8">
 		<ul class="slides">
